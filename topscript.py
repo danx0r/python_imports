e20aca75622ca1a605c462118c71668631bd2f36
danx0r <dan.miller@eye0.com>
2018-11-26 16:09:02 -0800

    abs import deep2

--- topscript.py
+++ topscript.py
@@ -1,5 +1,8 @@
 from mod2 import code2
 print("loaded", code2)
 
-from mod2.deep import code3
-print("loaded", code3)
+# from mod2.deep import code3
+# print("loaded", code3)
+
+from mod2.deep2 import code4
+print("loaded", code4)
