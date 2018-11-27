27d5e634360afe33af10f5a8b48427741cc0bfed
danx0r <dan.miller@eye0.com>
2018-11-26 16:02:43 -0800

    abs import deep

---
+++ mod2/deep/code3.py
@@ -0,0 +1,4 @@
+from mod1 import code1
+print("I am code3")
+print("imported:", code1)
+
--- topscript.py
+++ topscript.py
@@ -1,2 +1,5 @@
 from mod2 import code2
 print("loaded", code2)
+
+from mod2.deep import code3
+print("loaded", code3)
